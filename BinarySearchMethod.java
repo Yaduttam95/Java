4167b8d7516e874766a2976b37faefa56fe12618
Yaduttam Pareek <yaduttampareek95@gmail.com>
2022-10-19 05:27:33 +0530

    Revert "Update BinarySearchMethod.java"

--- BinarySearchMethod.java
+++ BinarySearchMethod.java
@@ -1,16 +1,12 @@
 import java.util.Arrays;
-// Main method start here
+
 class BinarySearchMethod{
-	// Main method
 	public static void main(String []args){
-		// Array initialization
 		int a[]={10,3,5,85,65,25};
-		// Sorting of array
 		Arrays.sort(a);
-		// For loop
 		for(int e:a)
 			System.out.println(e);
-		// Printing of the binary search
+		
 		System.out.println(Arrays.binarySearch(a,6));
 	}
 }
