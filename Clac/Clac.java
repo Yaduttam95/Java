f8cbb3e82f2bce2735e194028014158c1a7cf29e
raman04-byte <ramantank04022002@gmail.com>
2022-10-19 11:29:19 +0530

    Create Clac.java

---
+++ Clac/Clac.java
@@ -0,0 +1,58 @@
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calc extends Applet implements ActionListener {
+
+    TextField t1 = new TextField(10);
+    TextField t2 = new TextField(10);
+    TextField t3 = new TextField(10);
+    Label l1 = new Label("First no. ");
+    Label l2 = new Label("Second no. ");
+    Label l3 = new Label("Res ");
+    Button b1 = new Button("Add");
+    Button b2 = new Button("Sub");
+    Button b3 = new Button("Mul");
+    Button b4 = new Button("Div");
+
+    public void init() {
+        add(l1);
+        add(t1);
+        add(l2);
+        add(t2);
+        add(l3);
+        add(t3);
+        add(b1);
+        add(b2);
+        add(b3);
+        add(b4);
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        b3.addActionListener(this);
+        b4.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == b1) {
+            int n1 = Integer.parseInt(t1.getText());
+            int n2 = Integer.parseInt(t2.getText());
+            t3.setText(" " + (n1 + n2));
+        }
+        if (e.getSource() == b2) {
+            int n1 = Integer.parseInt(t1.getText());
+            int n2 = Integer.parseInt(t2.getText());
+            t3.setText(" " + (n1 - n2));
+        }
+        if (e.getSource() == b3) {
+            int n1 = Integer.parseInt(t1.getText());
+            int n2 = Integer.parseInt(t2.getText());
+            t3.setText(" " + (n1 * n2));
+        }
+        if (e.getSource() == b4) {
+            int n1 = Integer.parseInt(t1.getText());
+            int n2 = Integer.parseInt(t2.getText());
+            t3.setText(" " + (n1 / n2));
+        }
+    }
+
+}
