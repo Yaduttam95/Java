046e4ef992931b32f25e29bc3f7107eab904eeed
Tusharr0305 <113424554+Tusharr0305@users.noreply.github.com>
2022-10-29 00:01:26 +0530

    Create Countpairs.cpp
    compleated every step(readme/starting instruction) for adding program

---
+++ Countpairs.cpp
@@ -0,0 +1,27 @@
+// Java implementation of simple method to find count of
+// pairs with given sum.
+public class find {
+	public static void main(String args[])
+	{
+		int[] arr = { 1, 5, 7, -1, 5 };
+		int sum = 6;
+		getPairsCount(arr, sum);
+	}
+
+	// Prints number of pairs in arr[0..n-1] with sum equal
+	// to 'sum'
+	public static void getPairsCount(int[] arr, int sum)
+	{
+
+		int count = 0; // Initialize result
+
+		// Consider all possible pairs and check their sums
+		for (int i = 0; i < arr.length; i++)
+			for (int j = i + 1; j < arr.length; j++)
+				if ((arr[i] + arr[j]) == sum)
+					count++;
+
+		System.out.printf("Count of pairs is %d", count);
+	}
+}
+
